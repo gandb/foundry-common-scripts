4fd4cddf17bb68b6f80739d0dbdcc4512b82da9d
gandb <gandnegro@gmail.com>
2025-12-17 19:59:16 +0000

    correcao do hide unidentify

--- src/hide-unidentify.js
+++ src/hide-unidentify.js
@@ -1,130 +1,130 @@
-const commonModule = document.COMMON_MODULE;
 
-commonModule.hideUnidentify = {
-  removeButtonsFromItemContext: (item,buttons)=>
-   {
-      commonModule.debug("removeButtonsFromItemContext called");
-       if (game.user.isGM) {
-        if(!commonModule.debugMode)
-        {
-          commonModule.debug("is GM - not removing Identify button from Item Sheet context menu");
-          return;
-        }
-        commonModule.debug("is GM in debug mode - removing Identify button from Item Sheet context menu");
 
-      }
-      const identified = item.system.identified ;
-      if (identified){
-        commonModule.debug("Item:",item.name," - Item is identified - not removing Identify button from Item Sheet context menu");
-        return;
-      } 
-      
-      const needRemoveButtons = ["DND5E.Identify","DND5E.ContextMenuActionAttune"];
-      
-      needRemoveButtons.forEach( buttonName => {
-        const index = buttons.findIndex(option => option.name === buttonName);
-        if(index !== -1){
-          commonModule.debug("Item:",item.name," - Removing",buttonName,"button from Item Sheet context menu at index",index);
-          buttons.splice(index, 1);
-        }
-      });
-   },
-   removeItemSheetIdentifyInformations: (sheet,[html])=>{
-      //console.log("renderItemSheet5e");
-    
-      if (game.user.isGM) 
-      {
-        if(!commonModule.debugMode)
-        {
-          commonModule.debug("removing Info from Item Sheet");
-          return;
-        }
-        commonModule.debug("is GM in debug mode - removing information from Item Sheet");
-      }
-    
-    
-      const identified = sheet.item.system.identified ;
-      if (identified){
-        return;
-      } 
 
-      document.querySelectorAll(".dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))").forEach(n => n.remove());
-      document.querySelectorAll(".toggle-identified").forEach(n=>n.remove()); 
-      
+function createHideUnidentify(){
+
  
- 
-   },
-   removeCharacterSheetIdentifyInformation: (sheet)=>{
-      //console.log("renderItemSheet5e");
-     
-      if (game.user.isGM) 
-      {
-        if(!commonModule.debugMode)
-        {
-          commonModule.debug("removing Info from Item Sheet");
-          return;
-        }
-        commonModule.debug("is GM in debug mode - removing information from Item Sheet");
-      } 
 
-      commonModule.hideUnidentify.sheetTime = setInterval(()=>{
+  document.COMMON_MODULE.HIDE_UNIDENTIFY = {
+    removeButtonsFromItemContext: (item,buttons)=>
+    {
+        document.COMMON_MODULE.debug("removeButtonsFromItemContext called");
+        if (game.user.isGM) {
+          if(!document.COMMON_MODULE.debugMode)
+          {
+            document.COMMON_MODULE.debug("is GM - not removing Identify button from Item Sheet context menu");
+            return;
+          }
+          document.COMMON_MODULE.debug("is GM in debug mode - removing Identify button from Item Sheet context menu");
+
+        }
+        const identified = item.system.identified ;
+        if (identified){
+          document.COMMON_MODULE.debug("Item:",item.name," - Item is identified - not removing Identify button from Item Sheet context menu");
+          return;
+        } 
         
-          commonModule.debug("sheet opened - removing identify information from Character Sheet");
-          const allItens = document.querySelectorAll(".item-list .item-row");
-          
-      },250); 
-   },
-  };
+        const needRemoveButtons = ["DND5E.Identify","DND5E.ContextMenuActionAttune"];
+        
+        needRemoveButtons.forEach( buttonName => {
+          const index = buttons.findIndex(option => option.name === buttonName);
+          if(index !== -1){
+            document.COMMON_MODULE.debug("Item:",item.name," - Removing",buttonName,"button from Item Sheet context menu at index",index);
+            buttons.splice(index, 1);
+          }
+        });
+    },
+    removeItemSheetIdentifyInformations: (sheet,[html])=>{
+        //console.log("renderItemSheet5e");
+      
+        if (game.user.isGM) 
+        {
+          if(!document.COMMON_MODULE.debugMode)
+          {
+            document.COMMON_MODULE.debug("removing Info from Item Sheet");
+            return;
+          }
+          document.COMMON_MODULE.debug("is GM in debug mode - removing information from Item Sheet");
+        }
+      
+      
+        const identified = sheet.item.system.identified ;
+        if (identified){
+          return;
+        } 
 
-
-   
- 
- //============HOOKS================== 
-// for dnd5e version 3.3.0+
-// Prevent Players from Identifying their items by hiding the buttons from them, only the GM will be able to Identify them
-// Remove Identify button at top of Item Sheet
-Hooks.on("renderItemSheet5e", (sheet, [html]) => {
-  commonModule.debug("dnd5e.renderItemSheet5e called");
-  commonModule.hideUnidentify.removeItemSheetIdentifyInformations(sheet, [html]);
-});
-
-Hooks.on("closeCharacterActorSheet", (sheet) => {
-  commonModule.debug("dnd5e.closeCharacterActorSheet called"); 
-  clearInterval(commonModule.hideUnidentify.sheetTime);
-  commonModule.debug("timer desligado"); 
-  commonModule.hideUnidentify.sheetTime = null;
-});
-
-
-Hooks.on("renderActorSheet5eCharacter", (sheet, [html]) => {
-  commonModule.debug("dnd5e.renderActorSheet5eCharacter called"); 
-});
-
-
-// Remove Identify button from Item Context menu on Actor Sheet
-Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
-     commonModule.debug("dnd5e.getItemContextOptions called");
-     commonModule.hideUnidentify.removeButtonsFromItemContext(item,buttons);
-});
-
-
-Hooks.on("renderDocumentSheetV2", (sheet) => { 
-  commonModule.debug("renderDocumentSheetV2 called with parameters:",sheet);
-  commonModule.hideUnidentify.removeCharacterSheetIdentifyInformation(sheet);
-});
-
+        document.querySelectorAll(".dnd5e.sheet.item .sheet-header .item-subtitle label:has(input:not([disabled]))").forEach(n => n.remove());
+        document.querySelectorAll(".toggle-identified").forEach(n=>n.remove()); 
+        
+  
+  
+    },
+    removeCharacterSheetIdentifyInformation: (sheet)=>{
+        //console.log("renderItemSheet5e");
+      
+        if (game.user.isGM) 
+        {
+          if(!document.COMMON_MODULE.debugMode)
+          {
+            document.COMMON_MODULE.debug("removing Info from Item Sheet");
+            return;
+          }
+          document.COMMON_MODULE.debug("is GM in debug mode - removing information from Item Sheet");
+        } 
 /*
- 
-Hooks.on("init",   () => {
-  console.log("init","ready teste");
-  let item = game?.items?.find(item => item?.id == "Rj6fh9Sg59QTCHkm");
-  console.log("x777","Item",item);
+        //remover o attune por linha?
+        document.COMMON_MODULE.HIDE_UNIDENTIFY.sheetTime = setInterval(()=>{
+          
+            document.COMMON_MODULE.debug("sheet opened - removing identify information from Character Sheet");
+            const allItens = document.querySelectorAll(".item-list .item-row");
+            
+        },250); */
+    },
+    };
+
+
+    Hooks.on("renderItemSheet5e", (sheet, [html]) => {
+      document.COMMON_MODULE.debug("dnd5e.renderItemSheet5e called");
+      document.COMMON_MODULE.HIDE_UNIDENTIFY.removeItemSheetIdentifyInformations(sheet, [html]);
+    });
+
+    /*
+     //remover o attune por linha?
+    Hooks.on("closeCharacterActorSheet", (sheet) => {
+      document.COMMON_MODULE.debug("dnd5e.closeCharacterActorSheet called"); 
+      clearInterval(document.COMMON_MODULE.HIDE_UNIDENTIFY.sheetTime);
+      document.COMMON_MODULE.debug("timer desligado"); 
+      document.COMMON_MODULE.HIDE_UNIDENTIFY.sheetTime = null;
+    });
+
+    Hooks.on("renderActorSheet5eCharacter", (sheet, [html]) => {
+      document.COMMON_MODULE.debug("dnd5e.renderActorSheet5eCharacter called"); 
+    });
+
+
+    */
+
+
+
+    // Remove Identify button from Item Context menu on Actor Sheet
+    Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
+        document.COMMON_MODULE.debug("dnd5e.getItemContextOptions called");
+        document.COMMON_MODULE.HIDE_UNIDENTIFY.removeButtonsFromItemContext(item,buttons);
+    });
+
+
+    Hooks.on("renderDocumentSheetV2", (sheet) => { 
+      document.COMMON_MODULE.debug("renderDocumentSheetV2 called with parameters:",sheet);
+      document.COMMON_MODULE.HIDE_UNIDENTIFY.removeCharacterSheetIdentifyInformation(sheet);
+    });
+
+
+}
+
+console.log("teste 123");
+
+Hooks.on("onReadyCommonModule", ( ) => { 
+  document.COMMON_MODULE.debug("onReadyCommonModule called"); 
+  createHideUnidentify();
 });
  
-Hooks.on("ready",   () => {
-  console.log("ready","ready teste");
-  let item = game?.items?.find(item => item?.id == "Rj6fh9Sg59QTCHkm");
-  console.log("ready","Item",item);
- });
- 
- */
