cc3a54e3efd8c0425bc907ba5ab5fd9066ba4118
gandb <gandnegro@gmail.com>
2025-12-18 14:19:33 +0000

    conversão do module.js e typescript

--- src/ global.d.ts
+++ src/ global.d.ts
@@ -4,6 +4,7 @@ declare const Hooks: {
   once(event: string, callback: (...args: unknown[]) => void): number;
   off(event: string, callback: (...args: unknown[]) => void): number;
   call(event: string, ...args: unknown[]): unknown;
+  callAll(event:string, ...args: unknown[]): unknown;
 };
 
 declare const CONFIG = {
@@ -29,6 +30,101 @@ declare const AudioHelper:{
 
 private class Game{
 }
+private class Ui{
+}
+
+private class ChatInfo{
+    content:string;
+    speaker:Speaker;
+};
+
+private class SpeakerInfo{
+    alias:string;
+};
+
+private class Speaker{
+    
+};
+
+private  class PlayInfo{
+    src:string;
+    autoplay:boolean;
+}
+
+// Declara o valor global que o Foundry injeta em runtime
+declare const Hooks: {
+  on(event: string, callback: (...args: unknown[]) => void): number;
+  once(event: string, callback: (...args: unknown[]) => void): number;
+  off(event: string, callback: (...args: unknown[]) => void): number;
+  call(event: string, ...args: unknown[]): unknown;
+};
+
+declare const ChatMessage:{
+    create( chatInfo: ChatInfo);
+    getSpeaker( speakerInfo:SpeakerInfo):any;
+};
+
+declare const game:any|Game = new Game();
+
+
+
+declare const ui:any|Ui = new Ui();
+
+declare const AudioHelper:{
+    preloadSound(path:string):Promise<void>;
+    play(playInfo:PlayInfo,autoplay:boolean):void;
+}; 
+
+declare class FoundryDocument extends Document{
+    COMMON_MODULE:CommonModule;
+
+}
+
+
+declare class NPCDialog {
+
+    npcSelected:NPC|any;
+    activeNPC:NPC|any;
+    npcs:Map<string,NPC>=new Map();
+
+
+    async addNPCButtons (controls:any) ;
+
+    async showNPCChooseDialog ();
+
+    async callMinsc (frmModule:Module);
+
+    async warnAboutUpdate  (lastVersion:any);
+
+}  
+
+
+
+declare const docs:any|FoundryDocument ;
+
+declare class Module{}
+  
+ 
+private class Screen{
+    public name:string;
+    public type:string;
+    public callback:any;
+}
+
+private class DialogUtils{
+    public helpSubmit:string;
+    public createDialog(title:string,style:string,content:string,buttons:Array<any>, callback:any=undefined);
+    public createButton (action:string,label:string,defaultValue:boolean,type:"screen"|"screen-context"|"action",callback:any=undefined);
+
+}
+ 
+
+
+
+private class Game{
+}
+
+p
 private class Ui{
 }
 
