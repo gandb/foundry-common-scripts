7d6a33146a761b5b7b443f71e4aa9fb507c03a27
Edson Vicente Carli Junior <gandnegro@gmail.com>
2026-04-08 16:36:13 +0100

    configuracao de acordo com o arquivo

--- src/module.ts
+++ src/module.ts
@@ -5,14 +5,24 @@ import { NPC, NPCDialog, DialogUtils, ModuleBase, SubModuleBase } from "./";
 
 const commonModule = new CommonModule();
 const doc = document as FoundryDocument;
-const logguer: Logguer = new Logguer({ format: "", prefix: "CA", hasDate: true, hasLevel: true });
 
-injectController.registerByName("FoundryDocument", doc);
-injectController.registerByName("CommonModule", commonModule);
-injectController.registerByName("CommonLogguer", logguer);
+async function initModule() {
+    const configResponse = await fetch('../config.json');
+    const config = await configResponse.json();
+    const logConfig = config.log || { format: "", prefix: "CA", hasDate: true, hasLevel: true };
+    
+    const logguer: Logguer = new Logguer(logConfig);
 
+    logguer.level(Level.DEBUG);
 
-commonModule.init();
+    injectController.registerByName("FoundryDocument", doc);
+    injectController.registerByName("CommonModule", commonModule);
+    injectController.registerByName("CommonLogguer", logguer);
+
+    commonModule.init();
+}
+
+initModule();
 
 (window as any).TaulukkoCommon = {
     NPC,
